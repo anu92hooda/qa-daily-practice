e66e4fb49d07f1f316f1f3913724efdf605472b8
anu92hooda <anu92hooda@gmail.com>
2025-06-29 17:01:38 -0400

    chnages to refect deeted file

---
+++ python_basics/Even_Odd_Splitter.py
@@ -0,0 +1,21 @@
+def even_odd():
+
+    evens=[]
+    odds=[]
+
+    for i in range(1,30):
+
+        if i%2==0:
+            evens.append(i)
+
+        else:
+            odds.append(i)
+
+    print(evens)
+    print(odds)
+
+even_odd()
+
+
+
+
